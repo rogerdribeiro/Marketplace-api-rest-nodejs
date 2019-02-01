1b8f2c4b13b22fff0415e17f92345af12fc3ca65
Rogerd Ribeiro <rogerd.ribeiro@aluno.ufop.edu.br>
2019-02-01 11:11:38 -0200

    validação e exception handler

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -1,6 +1,7 @@
 const Ad = require('../models/Ad')
 const User = require('../models/User')
-const Mail = require('../services/Mail')
+const Queue = require('../services/Queue')
+const PurchaseMail = require('../jobs/PurchaseMail')
 
 class PurchaseController {
   async store (req, res) {
@@ -9,13 +10,11 @@ class PurchaseController {
     const purchaseAd = await Ad.findById(ad).populate('author')
     const user = await User.findById(req.userId)
 
-    await Mail.sendMail({
-      from: '"Rogerd Ribeiro" <rogerd@ribeiro.com>',
-      to: purchaseAd.author.email,
-      subject: `Solicitação de compra: ${purchaseAd.title}`,
-      html: `<p>Teste:${content}</p>`
-    })
-
+    Queue.create(PurchaseMail.key, {
+      ad: purchaseAd,
+      user,
+      content
+    }).save()
     return res.send()
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const validate = require('express-validation')
+const Youch = require('youch')
 const databaseConfig = require('./config/database')
+
 class App {
   constructor () {
     this.express = express()
@@ -9,22 +12,36 @@ class App {
     this.database()
     this.middlewares()
     this.routes()
+    this.exception()
   }
 
   database () {
-    mongoose.connect(
-      databaseConfig.uri,
-      {
-        useCreateIndex: true,
-        useNewUrlParser: true
-      }
-    )
+    mongoose.connect(databaseConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
   }
   middlewares () {
     this.express.use(express.json())
   }
   routes () {
     this.express.use(require('./routes'))
+  }
+  exception () {
+    this.express.use(async (err, req, res, next) => {
+      if (err instanceof validate.ValidationError) {
+        return res.status(err.status).json(err)
+      }
+
+      if (process.env.NODE_ENV !== 'production') {
+        const youch = new Youch(err)
+        return res.json(await youch.toJSON())
+      }
+
+      return res
+        .status(err.status || 500)
+        .json({ error: 'Internal Server Error' })
+    })
   }
 }
 
